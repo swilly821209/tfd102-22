28187d2c6834ab1ad0b773e53c9938de9eea114a
swilly821209 <swilly821209@icloud.com>
2021-05-16 22:47:14 +0800

    find.html

--- js/findastore.js
+++ js/findastore.js
@@ -1,3 +1,33 @@
-import {socialIcon} from './iconscale.js'
+import { socialIcon } from './iconscale.js'
 import { sliderPosition } from './slider.js'
-
+const container = document.querySelector('.store-container');
+async function filterStore(text) {
+    const res = await fetch('./js/store.json')
+    const data = await res.json()
+    let str = ''
+    data.filter((li) => {
+        if (text == '') {
+            str += `<tr>
+            <td>${li.no}</td>
+            <td>${li.location}</td>
+            <td>${li.address}</td>
+            <td>${li.businessHour}</td>
+            <td>${li.number}</td>
+        </tr>`
+        } else if (li.location.toLowerCase().includes(text) || li.address.toLowerCase().includes(text)) {
+            str += `<tr>
+            <td>${li.no}</td>
+            <td>${li.location}</td>
+            <td>${li.address}</td>
+            <td>${li.businessHour}</td>
+            <td>${li.number}</td>
+        </tr>`
+        }
+    })
+    container.innerHTML = str
+}
+filterStore('')
+const input = document.querySelector('.find-input')
+input.addEventListener('input', () => {
+    filterStore(input.value)
+})
